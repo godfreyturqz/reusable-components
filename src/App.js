6f6ab6ef681ef1efff503878ace8f3512ff00a7f
godfreyturqz <62551257+godfreyturqz@users.noreply.github.com>
2020-11-21 23:27:55 +0800

    added inputs component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import AllButtons from './components/Buttons/AllButtons';
 import AllCards from './components/Cards/AllCards';
 import AllImageContainers from './components/ImageContainer/AllImageContainers';
+import Inputs from './components/Inputs';
 
 
 function App() {
@@ -10,8 +11,8 @@ function App() {
       <h1>JUST PLAIN TEMPLATES</h1><br/>
       <h3>Author's notes:</h3><br/>
       <p>1. Parameters like width, padding and margins shall be set by the user.</p><br/>
-      <p>2. </p><br/>
       
+      <Inputs/>
       <AllButtons/>
       <AllCards/>
       <AllImageContainers/>
---
+++ src/components/Inputs/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './styles.css'
+
+function Inputs() {
+    return (
+        <div className="input">
+            <input type="text"/>
+        </div>
+    )
+}
+
+export default Inputs
